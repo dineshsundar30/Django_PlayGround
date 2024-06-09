ae7e38a8f47a62ceeb1b090627732cd31fb404cc
dineshsundar30 <dineshsundar73.gmail.com>
2024-06-09 10:46:41 +0530

    latest update

--- firstproject/firstproject/urls.py
+++ firstproject/firstproject/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import path,include
-
+handler404 = 'firstproject.views.pag_not_found'
 urlpatterns = [
     path("",include("demoap.urls")),
     path('admin/', admin.site.urls),
